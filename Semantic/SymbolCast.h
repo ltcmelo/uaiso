7406be3b98e1779425c54ff7fd36db512dcda90f
Leandro T. C. Melo <ltcmelo@gmail.com>
2015-12-12 08:46:58 -0200

    Symbol cast fix for const values

--- Semantic/SymbolCast.h
+++ Semantic/SymbolCast.h
@@ -26,31 +26,31 @@
 
 #define ConstAlias_Cast(T) static_cast<const Alias*>(T)
 #define Alias_Cast(T) static_cast<uaiso::Alias*>(T)
-#define Alias_ConstCast(T) const_cast<uaiso::Alias*>(Alias_Cast(T))
+#define Alias_ConstCast(T) const_cast<uaiso::Alias*>(ConstAlias_Cast(T))
 
 #define ConstBaseRecord_Cast(T) static_cast<const BaseRecord*>(T)
 #define BaseRecord_Cast(T) static_cast<uaiso::BaseRecord*>(T)
-#define BaseRecord_ConstCast(T) const_cast<uaiso::BaseRecord*>(BaseRecord_Cast(T))
+#define BaseRecord_ConstCast(T) const_cast<uaiso::BaseRecord*>(ConstBaseRecord_Cast(T))
 
 #define ConstBlock_Cast(T) static_cast<const Block*>(T)
 #define Block_Cast(T) static_cast<uaiso::Block*>(T)
-#define Block_ConstCast(T) const_cast<uaiso::Block*>(Block_Cast(T))
+#define Block_ConstCast(T) const_cast<uaiso::Block*>(ConstBlock_Cast(T))
 
 #define ConstDeclSymbol_Cast(T) static_cast<const DeclSymbol*>(T)
 #define DeclSymbol_Cast(T) static_cast<uaiso::DeclSymbol*>(T)
-#define DeclSymbol_ConstCast(T) const_cast<uaiso::DeclSymbol*>(DeclSymbol_Cast(T))
+#define DeclSymbol_ConstCast(T) const_cast<uaiso::DeclSymbol*>(ConstDeclSymbol_Cast(T))
 
 #define ConstEnum_Cast(T) static_cast<const Enum*>(T)
 #define Enum_Cast(T) static_cast<uaiso::Enum*>(T)
-#define Enum_ConstCast(T) const_cast<uaiso::Enum*>(Enum_Cast(T))
+#define Enum_ConstCast(T) const_cast<uaiso::Enum*>(ConstEnum_Cast(T))
 
 #define ConstFunc_Cast(T) static_cast<const Func*>(T)
 #define Func_Cast(T) static_cast<uaiso::Func*>(T)
-#define Func_ConstCast(T) const_cast<uaiso::Func*>(Func_Cast(T))
+#define Func_ConstCast(T) const_cast<uaiso::Func*>(ConstFunc_Cast(T))
 
 #define ConstParam_Cast(T) static_cast<const Param*>(T)
 #define Param_Cast(T) static_cast<uaiso::Param*>(T)
-#define Param_ConstCast(T) const_cast<uaiso::Param*>(Param_Cast(T))
+#define Param_ConstCast(T) const_cast<uaiso::Param*>(ConstParam_Cast(T))
 
 #define ConstRecord_Cast(T) static_cast<const Record*>(T)
 #define Record_Cast(T) static_cast<uaiso::Record*>(T)
@@ -58,14 +58,14 @@
 
 #define ConstTypeSymbol_Cast(T) static_cast<const TypeSymbol*>(T)
 #define TypeSymbol_Cast(T) static_cast<uaiso::TypeSymbol*>(T)
-#define TypeSymbol_ConstCast(T) const_cast<uaiso::TypeSymbol*>(TypeSymbol_Cast(T))
+#define TypeSymbol_ConstCast(T) const_cast<uaiso::TypeSymbol*>(ConstTypeSymbol_Cast(T))
 
 #define ConstValueSymbol_Cast(T) static_cast<const ValueSymbol*>(T)
 #define ValueSymbol_Cast(T) static_cast<uaiso::ValueSymbol*>(T)
-#define ValueSymbol_ConstCast(T) const_cast<uaiso::ValueSymbol*>(ValueSymbol_Cast(T))
+#define ValueSymbol_ConstCast(T) const_cast<uaiso::ValueSymbol*>(ConstValueSymbol_Cast(T))
 
 #define ConstVar_Cast(T) static_cast<const Var*>(T)
 #define Var_Cast(T) static_cast<uaiso::Var*>(T)
-#define Var_ConstCast(T) const_cast<uaiso::Var*>(Var_Cast(T))
+#define Var_ConstCast(T) const_cast<uaiso::Var*>(ConstVar_Cast(T))
 
 #endif
