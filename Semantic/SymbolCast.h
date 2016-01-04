b1f00bd1096a0d5e7e1fc66388527e06b1b4aebb
Leandro T. C. Melo <ltcmelo@gmail.com>
2016-01-04 17:50:22 -0200

    uaiso prefix in Symbol casts

--- Semantic/SymbolCast.h
+++ Semantic/SymbolCast.h
@@ -24,52 +24,52 @@
 #ifndef UAISO_SYMBOLCAST_H
 #define UAISO_SYMBOLCAST_H
 
-#define ConstAlias_Cast(T) static_cast<const Alias*>(T)
+#define ConstAlias_Cast(T) static_cast<const uaiso::Alias*>(T)
 #define Alias_Cast(T) static_cast<uaiso::Alias*>(T)
 #define Alias_ConstCast(T) const_cast<uaiso::Alias*>(ConstAlias_Cast(T))
 
-#define ConstBaseRecord_Cast(T) static_cast<const BaseRecord*>(T)
+#define ConstBaseRecord_Cast(T) static_cast<const uaiso::BaseRecord*>(T)
 #define BaseRecord_Cast(T) static_cast<uaiso::BaseRecord*>(T)
 #define BaseRecord_ConstCast(T) const_cast<uaiso::BaseRecord*>(ConstBaseRecord_Cast(T))
 
-#define ConstBlock_Cast(T) static_cast<const Block*>(T)
+#define ConstBlock_Cast(T) static_cast<const uaiso::Block*>(T)
 #define Block_Cast(T) static_cast<uaiso::Block*>(T)
 #define Block_ConstCast(T) const_cast<uaiso::Block*>(ConstBlock_Cast(T))
 
 // Name would conflict with AST's decl cast.
-#define ConstDeclSymbol_Cast(T) static_cast<const Decl*>(T)
+#define ConstDeclSymbol_Cast(T) static_cast<const uaiso::Decl*>(T)
 #define DeclSymbol_Cast(T) static_cast<uaiso::Decl*>(T)
 #define DeclSymbol_ConstCast(T) const_cast<uaiso::Decl*>(ConstDeclSymbol_Cast(T))
 
-#define ConstEnum_Cast(T) static_cast<const Enum*>(T)
+#define ConstEnum_Cast(T) static_cast<const uaiso::Enum*>(T)
 #define Enum_Cast(T) static_cast<uaiso::Enum*>(T)
 #define Enum_ConstCast(T) const_cast<uaiso::Enum*>(ConstEnum_Cast(T))
 
-#define ConstFunc_Cast(T) static_cast<const Func*>(T)
+#define ConstFunc_Cast(T) static_cast<const uaiso::Func*>(T)
 #define Func_Cast(T) static_cast<uaiso::Func*>(T)
 #define Func_ConstCast(T) const_cast<uaiso::Func*>(ConstFunc_Cast(T))
 
-#define ConstNamespace_Cast(T) static_cast<const Namespace*>(T)
+#define ConstNamespace_Cast(T) static_cast<const uaiso::Namespace*>(T)
 #define Namespace_Cast(T) static_cast<uaiso::Namespace*>(T)
 #define Namespace_ConstCast(T) const_cast<uaiso::Namespace*>(ConstNamespace_Cast(T))
 
-#define ConstParam_Cast(T) static_cast<const Param*>(T)
+#define ConstParam_Cast(T) static_cast<const uaiso::Param*>(T)
 #define Param_Cast(T) static_cast<uaiso::Param*>(T)
 #define Param_ConstCast(T) const_cast<uaiso::Param*>(ConstParam_Cast(T))
 
-#define ConstRecord_Cast(T) static_cast<const Record*>(T)
+#define ConstRecord_Cast(T) static_cast<const uaiso::Record*>(T)
 #define Record_Cast(T) static_cast<uaiso::Record*>(T)
 #define Record_ConstCast(T) const_cast<uaiso::Record*>(ConstRecord_Cast(T))
 
-#define ConstTypeDecl_Cast(T) static_cast<const TypeDecl*>(T)
+#define ConstTypeDecl_Cast(T) static_cast<const uaiso::TypeDecl*>(T)
 #define TypeDecl_Cast(T) static_cast<uaiso::TypeDecl*>(T)
 #define TypeDecl_ConstCast(T) const_cast<uaiso::TypeDecl*>(ConstTypeDecl_Cast(T))
 
-#define ConstValueDecl_Cast(T) static_cast<const ValueDecl*>(T)
+#define ConstValueDecl_Cast(T) static_cast<const uaiso::ValueDecl*>(T)
 #define ValueDecl_Cast(T) static_cast<uaiso::ValueDecl*>(T)
 #define ValueDecl_ConstCast(T) const_cast<uaiso::ValueDecl*>(ConstValueDecl_Cast(T))
 
-#define ConstVar_Cast(T) static_cast<const Var*>(T)
+#define ConstVar_Cast(T) static_cast<const uaiso::Var*>(T)
 #define Var_Cast(T) static_cast<uaiso::Var*>(T)
 #define Var_ConstCast(T) const_cast<uaiso::Var*>(ConstVar_Cast(T))
 
