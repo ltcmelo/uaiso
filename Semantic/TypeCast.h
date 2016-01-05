1ab3c3ad5fcccbc00274fc6a52052aae132556de
Leandro T. C. Melo <ltcmelo@gmail.com>
2016-01-05 20:04:37 -0200

    Constify Type cast macros

--- Semantic/TypeCast.h
+++ Semantic/TypeCast.h
@@ -26,63 +26,63 @@
 
 #define ConstArrayType_Cast(T) static_cast<const uaiso::ArrayType*>(T)
 #define ArrayType_Cast(T) static_cast<uaiso::ArrayType*>(T)
-#define ArrayType_ConstCast(T) const_cast<uaiso::ArrayType*>(ArrayType_Cast(T))
+#define ArrayType_ConstCast(T) const_cast<uaiso::ArrayType*>(ConstArrayType_Cast(T))
 
 #define ConstBoolType_Cast(T) static_cast<const uaiso::BoolType*>(T)
 #define BoolType_Cast(T) static_cast<uaiso::BoolType*>(T)
-#define BoolType_ConstCast(T) const_cast<uaiso::BoolType*>(BoolType_Cast(T))
+#define BoolType_ConstCast(T) const_cast<uaiso::BoolType*>(ConstBoolType_Cast(T))
 
 #define ConstChanType_Cast(T) static_cast<const uaiso::ChanType*>(T)
 #define ChanType_Cast(T) static_cast<uaiso::ChanType*>(T)
-#define ChanType_ConstCast(T) const_cast<uaiso::ChanType*>(ChanType_Cast(T))
+#define ChanType_ConstCast(T) const_cast<uaiso::ChanType*>(ConstChanType_Cast(T))
 
 #define ConstElaborateType_Cast(T) static_cast<const uaiso::ElaborateType*>(T)
 #define ElaborateType_Cast(T) static_cast<uaiso::ElaborateType*>(T)
-#define ElaborateType_ConstCast(T) const_cast<uaiso::ElaborateType*>(ConstElaborateType_Cast(T))
+#define ElaborateType_ConstCast(T) const_cast<uaiso::ElaborateType*>(ConstConstElaborateType_Cast(T))
 
 #define ConstEnumType_Cast(T) static_cast<const uaiso::EnumType*>(T)
 #define EnumType_Cast(T) static_cast<uaiso::EnumType*>(T)
-#define EnumType_ConstCast(T) const_cast<uaiso::EnumType*>(EnumType_Cast(T))
+#define EnumType_ConstCast(T) const_cast<uaiso::EnumType*>(ConstEnumType_Cast(T))
 
 #define ConstEnumType_Cast(T) static_cast<const uaiso::EnumType*>(T)
 #define EnumType_Cast(T) static_cast<uaiso::EnumType*>(T)
-#define EnumType_ConstCast(T) const_cast<uaiso::EnumType*>(EnumType_Cast(T))
+#define EnumType_ConstCast(T) const_cast<uaiso::EnumType*>(ConstEnumType_Cast(T))
 
 #define ConstFloatType_Cast(T) static_cast<const uaiso::FloatType*>(T)
 #define FloatType_Cast(T) static_cast<uaiso::FloatType*>(T)
-#define FloatType_ConstCast(T) const_cast<uaiso::FloatType*>(FloatType_Cast(T))
+#define FloatType_ConstCast(T) const_cast<uaiso::FloatType*>(ConstFloatType_Cast(T))
 
 #define ConstFuncType_Cast(T) static_cast<const uaiso::FuncType*>(T)
 #define FuncType_Cast(T) static_cast<uaiso::FuncType*>(T)
-#define FuncType_ConstCast(T) const_cast<uaiso::FuncType*>(FuncType_Cast(T))
+#define FuncType_ConstCast(T) const_cast<uaiso::FuncType*>(ConstFuncType_Cast(T))
 
 #define ConstIntType_Cast(T) static_cast<const uaiso::IntType*>(T)
 #define IntType_Cast(T) static_cast<uaiso::IntType*>(T)
-#define IntType_ConstCast(T) const_cast<uaiso::IntType*>(IntType_Cast(T))
+#define IntType_ConstCast(T) const_cast<uaiso::IntType*>(ConstIntType_Cast(T))
 
 #define ConstOpaqueType_Cast(T) static_cast<const uaiso::OpaqueType*>(T)
 #define OpaqueType_Cast(T) static_cast<uaiso::OpaqueType*>(T)
-#define OpaqueType_ConstCast(T) const_cast<uaiso::OpaqueType*>(OpaqueType_Cast(T))
+#define OpaqueType_ConstCast(T) const_cast<uaiso::OpaqueType*>(ConstOpaqueType_Cast(T))
 
 #define ConstPtrType_Cast(T) static_cast<const uaiso::PtrType*>(T)
 #define PtrType_Cast(T) static_cast<uaiso::PtrType*>(T)
-#define PtrType_ConstCast(T) const_cast<uaiso::PtrType*>(PtrType_Cast(T))
+#define PtrType_ConstCast(T) const_cast<uaiso::PtrType*>(ConstPtrType_Cast(T))
 
 #define ConstRecordType_Cast(T) static_cast<const uaiso::RecordType*>(T)
 #define RecordType_Cast(T) static_cast<uaiso::RecordType*>(T)
-#define RecordType_ConstCast(T) const_cast<uaiso::RecordType*>(RecordType_Cast(T))
+#define RecordType_ConstCast(T) const_cast<uaiso::RecordType*>(ConstRecordType_Cast(T))
 
 #define ConstStrType_Cast(T) static_cast<const uaiso::StrType*>(T)
 #define StrType_Cast(T) static_cast<StrType*>(T)
-#define StrType_ConstCast(T) const_cast<StrType*>(StrType_Cast(T))
+#define StrType_ConstCast(T) const_cast<StrType*>(ConstStrType_Cast(T))
 
 #define ConstSubrangeType_Cast(T) static_cast<const uaiso::SubrangeType*>(T)
 #define SubrangeType_Cast(T) static_cast<uaiso::SubrangeType*>(T)
-#define SubrangeType_ConstCast(T) const_cast<uaiso::SubrangeType*>(SubrangeType_Cast(T))
+#define SubrangeType_ConstCast(T) const_cast<uaiso::SubrangeType*>(ConstSubrangeType_Cast(T))
 
 #define ConstVoidType_Cast(T) static_cast<const uaiso::VoidType*>(T)
 #define VoidType_Cast(T) static_cast<uaiso::VoidType*>(T)
-#define VoidType_ConstCast(T) const_cast<uaiso::VoidType*>(VoidType_Cast(T))
+#define VoidType_ConstCast(T) const_cast<uaiso::VoidType*>(ConstVoidType_Cast(T))
 
 
 #endif
